1991d38ae3edd84ce0fa32ad55b41e2437b25129
Fallgregg <zabilska51@gmail.com>
2020-03-17 23:04:57 +0200

    Laboratory work number 3

---
+++ Lab03/MatrixClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab03 {
+    public class MatrixClass {
+        private int mult = 1;
+        public int n;
+        public int m;
+        public int[,] arr;
+        public MatrixClass(int a, int b, int[,] arr_temp) {
+            n = a;
+            m = b;
+            arr = new int[n, m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i, j] = arr_temp[i, j];
+                }
+            }
+        }
+
+        public int multiply {
+            get {
+                mult = 1;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        mult *= arr[i, j];
+                    }
+                }
+                return mult;
+            }
+        }
+        public double this[int index] {
+            get {
+                if (index >= n) {
+                    Console.WriteLine("Inputed index is out of the range.");
+                    return 0;
+                } else {
+                    double s = 0;
+                    for (int j = 0; j < m; j++) {
+                        s += arr[index, j];
+                    }
+                    s /= m;
+                    return s;
+                }
+
+            }
+        }
+
+    }
+}
+
---
+++ Lab03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab03;
+
+namespace Lab_03 {
+    class Progra {
+        static void Main() {
+            Console.WriteLine("Input n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[n, m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    Console.Write("Arr[" + (i + 1) + "," + (j + 1) + "] = ");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            MatrixClass arr_temp = new MatrixClass(n, m, arr);
+            Console.WriteLine("\nMultiplyed elements value: ");
+            Console.Write(arr_temp.multiply);
+            Console.WriteLine();
+            Console.WriteLine("\nInput Index: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            double sum = arr_temp[k - 1];
+            Console.Write("\nAvarage value: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
+
